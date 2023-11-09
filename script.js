bb982fefb717e22e8246c236036979ed0a0f11e3
davidsamuel-dev <davidsamuel.dev@gmail.com>
2023-11-09 16:35:51 -0300

    committed

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-alert("Ola mundo")
+
+function exibir_menu(){
+    // document.getElementById("menu_as").style.display = "block"
+    document.getElementById("menu_as").style.top = "0"
+   
+}
+function voltar_menu(){
+    var voltar = document.getElementById("menu_as").style.top = "-350px";
+    voltar.style.transition = "0.1s"
+}
