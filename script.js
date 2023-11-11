050b6b3e6398ebbf292f5ee920060c74d8cca037
davidsamuel-dev <davidsamuel.dev@gmail.com>
2023-11-11 10:31:39 -0300

    comited

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 function exibir_menu(){
     // document.getElementById("menu_as").style.display = "block"
     document.getElementById("menu_as").style.top = "0"
+    var voltar = document.getElementById("menu_as").style.position = "fixed"
+
    
 }
 function voltar_menu(){
